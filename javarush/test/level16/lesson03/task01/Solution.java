3a933ed27c9477a8f6800c2072eb045e394e41f2
Kutsanin Sergei <Snik91@live.ru>
2015-05-06 15:21:03 +0500

    Решены задачи level16.lesson3

--- javarush/test/level16/lesson03/task01/Solution.java
+++ javarush/test/level16/lesson03/task01/Solution.java
@@ -9,4 +9,14 @@ public class Solution {
         TestThread task = new TestThread();
         new Thread(task).start();
     }
+
+    public static class TestThread implements  Runnable
+    {
+
+        @Override
+        public void run()
+        {
+            System.out.println("My first thread");
+        }
+    }
 }
---
+++ javarush/test/level16/lesson03/task03/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level16.lesson03.task03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* Список и нити
+В методе main добавить в статический объект list 5 нитей SpecialThread - различных объектов.
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        //Add your code here - добавьте свой код тут
+        SpecialThread thread = new SpecialThread();
+        for(int i = 0; i < 5; i ++)
+            list.add(new Thread(thread));
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's run method inside SpecialThread");
+        }
+    }
+}
